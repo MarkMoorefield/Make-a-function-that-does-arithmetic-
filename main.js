7d7e263bee68228608e8a9235398489d38777b23
MarkMoorefield <155267237+MarkMoorefield@users.noreply.github.com>
2024-01-27 03:31:28 +0000

    .

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 function arithmetic(a, b, operator){
     if (operator === "add") { // If operator is equal to "add" in both value and type then its adds a + b.
       return a + b
-    } else if(operator === "subtract") {
+    } else if(operator === "subtract") { // If operator is equal to "subtract" in both value and type then its subtracts a and b.
       return a - b
     } else if (operator === "multiply") {
       return a * b
