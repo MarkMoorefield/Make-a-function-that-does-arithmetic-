11e5f2cb2e35c21ce836791b034ca4d05edaa696
MarkMoorefield <155267237+MarkMoorefield@users.noreply.github.com>
2024-01-27 03:30:51 +0000

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Made an else if statement that checked the operator sign. 
 // Depending on what the operator sign was it did the arithmetic based on it. 
 function arithmetic(a, b, operator){
-    if (operator === "add") {
+    if (operator === "add") { // If operator is equal to "add" in both value and type then its adds a + b.
       return a + b
     } else if(operator === "subtract") {
       return a - b
